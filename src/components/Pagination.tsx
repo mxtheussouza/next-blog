b18619ecc976327c1e686d2c07aa7a99d4a43d52
matheusdearaujo <matheusouzadearaujo@gmail.com>
2023-02-01 09:37:19 -0300

    build: add pre commit hooks

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,56 @@
+interface PaginationProps {
+  currentPage: number;
+  onPageNext: () => void;
+  onPageBack: () => void;
+}
+
+export default function Pagination({
+  currentPage,
+  onPageNext,
+  onPageBack,
+}: PaginationProps) {
+  return (
+    <div className="flex justify-center my-6 gap-4">
+      {currentPage > 1 && (
+        <button
+          className="inline-flex items-center bg-white hover:bg-gray-100 text-gray-800 font-bold py-2 px-4 border-b-4 border-gray-500 hover:border-gray-500 rounded shadow gap-2"
+          onClick={onPageBack}
+        >
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 mr-2"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+          Voltar
+        </button>
+      )}
+      <button
+        className="inline-flex items-center bg-white hover:bg-gray-100 text-gray-800 font-bold py-2 px-4 border-b-4 border-gray-500 hover:border-gray-500 rounded shadow gap-2"
+        onClick={onPageNext}
+      >
+        Próximo
+        <svg
+          aria-hidden="true"
+          className="w-5 h-5 ml-2"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+      </button>
+    </div>
+  );
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,7 +17,6 @@ export default function Document() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <body>
         <Main />
         <NextScript />
